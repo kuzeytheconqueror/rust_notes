1b77fd42b8c32823ab7c901d34800a37bb931c45
samliumay <samliumay965@gmail.com>
2026-08-20 15:17:39 +0300

    Functions added.

---
+++ codes/varialbes_05/target/debug/deps/varialbes_05-6197f163e44a2e50.d
@@ -0,0 +1,5 @@
+/home/umay-samli/Desktop/notes/programing_languages/Rust/codes/varialbes_05/target/debug/deps/varialbes_05-6197f163e44a2e50.d: src/main.rs
+
+/home/umay-samli/Desktop/notes/programing_languages/Rust/codes/varialbes_05/target/debug/deps/libvarialbes_05-6197f163e44a2e50.rmeta: src/main.rs
+
+src/main.rs:
